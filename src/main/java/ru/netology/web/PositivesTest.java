7cdc33b47c413846fa4f68ecaf06c728786e4c4e
Andrej-ori <a31338805@gmail.com>
2022-09-29 22:25:32 +0300

    add: NegativesTest.java modified: PositivesTest.java

--- src/main/java/ru/netology/web/PositivesTest.java
+++ src/main/java/ru/netology/web/PositivesTest.java
@@ -45,4 +45,19 @@ public class PositivesTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    // проверка двойной фамилии
+    public void shouldPositiveTest1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов-Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79991234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
 }
